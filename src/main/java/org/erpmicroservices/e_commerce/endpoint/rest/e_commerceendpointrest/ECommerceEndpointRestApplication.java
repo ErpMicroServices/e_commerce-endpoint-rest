3a1adfb8081fe232170a79031bc51f6fb6279c82
Jim Barrows <Jim.Barrows@gmail.com>
2021-03-29 14:01:32 -0500

    Initial commit.

---
+++ src/main/java/org/erpmicroservices/e_commerce/endpoint/rest/e_commerceendpointrest/ECommerceEndpointRestApplication.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.e_commerce.endpoint.rest.e_commerceendpointrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceEndpointRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceEndpointRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/erpmicroservices/e_commerce/endpoint/rest/e_commerceendpointrest/ECommerceEndpointRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.e_commerce.endpoint.rest.e_commerceendpointrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceEndpointRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
